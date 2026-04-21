d0d930ad7b61f81ed76d56deffae42589b65f24d
bbw1081 <181002857+bbw1081@users.noreply.github.com>
2026-04-21 17:59:14 -0400

    Added license info to python scripts

--- encode_descriptions.py
+++ encode_descriptions.py
@@ -1,3 +1,21 @@
+"""
+    encode_descriptions.py; parses and encodes the preset files
+    Copyright (C) 2026  Richard Wilkinson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from sentence_transformers import SentenceTransformer
 from pathlib import Path
 import json
